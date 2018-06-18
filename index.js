e936dc7807544d51c0cbe30ab6ffdfcb66037fa7
Benjamin Robinet <benjamin.robinet@free.fr>
2018-06-18 15:19:14 +0200

    update

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = require('express')(http);
 app.use(bodyParser.json);
 
 app.post('/', function (req, res) {
-    console.log(req.body)
+    console.log(req.body);
+    console.log('new req');
 });
 
 app.listen(2727, function () {
